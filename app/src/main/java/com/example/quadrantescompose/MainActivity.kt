92364c80ff39760322afd89e62e79ad26bd02897
Caíque Brener <brenercaique0806@gmail.com>
2022-11-28 00:09:33 -0300

    Create the composables

--- app/src/main/java/com/example/quadrantescompose/MainActivity.kt
+++ app/src/main/java/com/example/quadrantescompose/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.quadrantescompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quadrantescompose.ui.theme.QuadrantesComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +23,132 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QuadrantesComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    QuadranteComposable()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun QuadranteComposable() {
+    CardWithInfos(
+        color = Color.Green,
+        color2 = Color.Yellow,
+        color3 = Color.Cyan,
+        color4 = Color.LightGray
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    QuadrantesComposeTheme {
-        Greeting("Android")
+fun CardWithInfos(color: Color, color2: Color, color3: Color, color4: Color) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Row(
+            Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .fillMaxWidth()
+        ) {
+            Column(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(color)
+            ) {
+                CardInformations(
+                    title = stringResource(id = R.string.first_title), subtitle = stringResource(
+                        id = R.string.first_description
+                    )
+                )
+
+            }
+            Column(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(color2)
+            ) {
+                CardInformations(
+                    title = stringResource(id = R.string.second_title), subtitle = stringResource(
+                        id = R.string.second_description
+                    )
+                )
+            }
+
+        }
+        Row(
+            Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .fillMaxWidth()
+        ) {
+            Column(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(color3)
+            ) {
+                CardInformations(
+                    title = stringResource(id = R.string.third_title), subtitle = stringResource(
+                        id = R.string.third_description
+                    )
+                )
+            }
+            Column(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(color4)
+            ) {
+                CardInformations(
+                    title = stringResource(id = R.string.fourth_title), subtitle = stringResource(
+                        id = R.string.fourth_description
+                    )
+                )
+            }
+
+
+        }
     }
+
+}
+
+@Composable
+fun CardInformations(title: String, subtitle: String) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .wrapContentWidth()
+            .wrapContentHeight()
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(text = subtitle, textAlign = TextAlign.Justify)
+
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun QuadranteComposablePreview() {
+    QuadranteComposable()
 }
