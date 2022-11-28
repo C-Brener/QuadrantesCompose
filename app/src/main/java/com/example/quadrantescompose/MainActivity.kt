943f1c496ae727d23991b2e5e2b2d3fca76399bf
Caíque Brener <brenercaique0806@gmail.com>
2022-11-28 00:27:52 -0300

    Create Tests

--- app/src/main/java/com/example/quadrantescompose/MainActivity.kt
+++ app/src/main/java/com/example/quadrantescompose/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -64,6 +65,7 @@ fun CardWithInfos(color: Color, color2: Color, color3: Color, color4: Color) {
                     .fillMaxHeight()
                     .fillMaxWidth()
                     .background(color)
+                    .testTag("First Column")
             ) {
                 CardInformations(
                     title = stringResource(id = R.string.first_title), subtitle = stringResource(
@@ -78,6 +80,8 @@ fun CardWithInfos(color: Color, color2: Color, color3: Color, color4: Color) {
                     .fillMaxHeight()
                     .fillMaxWidth()
                     .background(color2)
+                    .testTag("Second Column")
+
             ) {
                 CardInformations(
                     title = stringResource(id = R.string.second_title), subtitle = stringResource(
@@ -99,6 +103,8 @@ fun CardWithInfos(color: Color, color2: Color, color3: Color, color4: Color) {
                     .fillMaxHeight()
                     .fillMaxWidth()
                     .background(color3)
+                    .testTag("Third Column")
+
             ) {
                 CardInformations(
                     title = stringResource(id = R.string.third_title), subtitle = stringResource(
@@ -112,6 +118,8 @@ fun CardWithInfos(color: Color, color2: Color, color3: Color, color4: Color) {
                     .fillMaxHeight()
                     .fillMaxWidth()
                     .background(color4)
+                    .testTag("Fourth Column")
+
             ) {
                 CardInformations(
                     title = stringResource(id = R.string.fourth_title), subtitle = stringResource(
--- app/src/test/java/com/example/quadrantescompose/MainActivityTest.kt
+++ app/src/test/java/com/example/quadrantescompose/MainActivityTest.kt
@@ -1,6 +1,45 @@
 package com.example.quadrantescompose
 
+import androidx.activity.compose.setContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.core.app.ActivityScenario
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowLog
 
+
+@RunWith(RobolectricTestRunner::class)
 class MainActivityTest{
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        ShadowLog.stream = System.out // Redirect Logcat to console
+    }
+
+    @Test
+    fun `Ensure the component init informations in the screen`(){
+        ActivityScenario.launch(MainActivity::class.java).use{ activityScenario ->
+
+            activityScenario.onActivity { activity ->
+
+
+                composeRule.onNodeWithTag("First Column").assertIsDisplayed()
+
+            }
+
+        }
+    }
+
+
 
 }
